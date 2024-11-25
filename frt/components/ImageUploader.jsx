e3f4154964b652b70488b15c703813e2076ecc44
CerinSusanPrakash <cerinsusan2002@gmail.com>
2024-11-25 20:39:30 +0530

    Update ImageUploader.jsx

--- frt/components/ImageUploader.jsx
+++ frt/components/ImageUploader.jsx
@@ -16,7 +16,8 @@ const ImageUploader = () => {
         formData.append('image', image);
 
         try {
-            const response = await axios.post('https://imgupload-api.vercel.app/api/images/upload', formData);
+            const response = await axios.post('https://imgupload-api.vercel.app/images/upload', formData);
+            // const response = await axios.post('https://imgupload-api.vercel.app/api/images/upload', formData);
             alert('Image uploaded successfully!');
             console.log(response.data);
         } catch (err) {
